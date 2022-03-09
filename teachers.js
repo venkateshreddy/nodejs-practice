76035e728e0a4d4106ed9730e0931e64a1c2e2c3
Saikrishnakmnr <saikrishna4234@gmail.com>
2022-03-09 17:48:50 +0530

    some files updated of teachers api files

---
+++ teachers.js
@@ -0,0 +1,3 @@
+[{ "id": "1", "name": "ramesh", "subject": "english", "gender": "male", "age": 32, "yoe": "3yr" },
+ { "id": 2, "name": "suresh", "subject": "english", "gender": "male", "age": 35, "yoe": "10yr" },
+  {"id": 3, "name": "aadarsha", "subject": "hindi", "gender": "female", "age": 35, "yoe": "8yr" }]
