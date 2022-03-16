690af29762f1a2ca1008ae727ad3718e6232592a
Srivani44 <srivanidayyala1@gmail.com>
2022-03-16 12:08:29 +0530

    Implemented Srivani-api Using mongoose

---
+++ students-mongoo.js
@@ -0,0 +1,101 @@
+const express=require('express');
+const application=express();
+application.use(express.json());
+application.use(express.urlencoded({ extended:true }));
+const mongoose=require("mongoose");
+
+//connect to mongoose
+ mongoose.connect("mongodb://localhost/college-management").then(()=>{
+ console.log("Connected to mongodb");
+ })
+ .catch(err=>{
+ console.log("err");
+ })
+//this is student schema.how the data should like in the students collections
+   const studentsSchema = new mongoose.Schema({
+      name:{
+         type:String
+      },
+      gender:{
+         type:String
+      },
+      dob:{
+         type:Date
+      },
+      branch:{
+         type:String
+      }
+   })
+   //this is students model which connects the table and schema
+    const studentsModel = mongoose.model('students',studentsSchema);
+    studentsModel.create({name:"Ram", gender:"Male","dob":new Date("12/5/2000"),branch:"EEE"})
+     studentsModel.create({name:"Laxman", gender:"Male",branch:"ECE"})
+   
+
+application.get('/',function (req,res){
+res.send({message:"Welcome to the college management system"});
+})
+
+application.get('/students',function (req,res){
+studentsModel.find({},(err,data)=>{
+   if(err){
+      res,send({error:true,message:"Unable to fetch the data"})
+   }else{
+      res.send(data);
+   }
+})
+})
+
+application.get('/students/search-by-gender',function (req,res){
+studentsModel.find({gender:req.query.gender},(err,data)=>{
+if(err){
+   res.send({error:true,message:"Unable to search by gender"})
+}else{
+   res.send(data)
+}
+})
+})
+
+application.get('/students/search-by-branch/:branch',function (req,res){
+   studentsModel.find({branch:req.params.branch},(err,data)=>{
+   if(err){
+      res.send({error:true,message:"Unable to search by gender"})
+   }else{
+      res.send(data)
+   }
+   })
+})
+
+application.post('/students/create',(req,res)=>{
+const newEmployee={...req.body}
+studentsModel.create(req.body,(err,data)=>{
+if(err){
+res.send({error:true,message:"Unable to insert student record"})
+}else{
+res.send(data)
+}
+})
+})
+ 
+application.put("/students/update/:id",(req,res)=>{ 
+studentsModel.findByIdAndUpdate(req.params.id,req.body,{new:true},(err,data)=>{
+if(err){
+res.send({error:true,message:"Unable to update student record"})
+}else{
+res.send(data);
+}
+})
+})
+
+application.delete("/students/delete/:id",(req,res)=>{  
+studentsModel.findByIdAndRemove(req.params.id,(err,data)=>{
+if(err){
+res.send({error:true,message:"Unable to update delete record"})
+}else{
+res.send(data);
+} 
+})
+})
+
+application.listen(4000)
+
